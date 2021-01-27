507aa7caf02567e9944aae33917e9b45d5282dec
Kalyani Bholane <77957208+kabmkb@users.noreply.github.com>
2021-01-27 12:48:48 -0600

    Delete Python.py

--- Python.py
+++
@@ -1,6 +0,0 @@
-print("Python")
-A = 'Python'        #Taking Python as a input
-B = A[1:5]          #Deleting first two characters
-print(B)
-C = B[::-1]         #Reversing the resultant string
-print(C)
