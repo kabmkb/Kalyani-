6edb8815006fd1dcf2a4392749a3965a7afcd360
Kalyani Bholane <77957208+kabmkb@users.noreply.github.com>
2021-02-05 20:44:51 -0600

    Delete ICP3/Sourcecode directory

--- ICP3/Sourcecode/Third_program.py
+++
@@ -1,7 +0,0 @@
-#Third program
-# importing numpy library
-import numpy as np
-a=np.random.randint(20, size=(4,5) )        #creating random vector of range 20 and reshaping it to the 4X5 size
-print(a)
-a[np.where(a==np.max(a,axis=1,keepdims=True))]=0    #replacing the max no. in a row by 0
-print(a)
