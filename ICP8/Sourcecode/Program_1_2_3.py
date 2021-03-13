c4eddea705ce83b31574f8a369e5a74b61eb439f
Kalyani Bholane <77957208+kabmkb@users.noreply.github.com>
2021-03-13 10:38:53 +0530

    Add files via upload

---
+++ ICP8/Sourcecode/Program_1_2_3.py
@@ -0,0 +1,91 @@
+
+# question 1 
+
+import pandas
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+
+# load dataset
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+
+dataset = pd.read_csv("diabetes.csv", header=None).values
+
+X_train, X_test, Y_train, Y_test = train_test_split(dataset[:,0:8], dataset[:,8],
+                                                    test_size=0.25, random_state=87)
+np.random.seed(155)
+my_first_nn = Sequential() # create model
+my_first_nn.add(Dense(20, input_dim=8, activation='relu')) # hidden layer
+my_first_nn.add(Dense(10, activation='relu')) # hidden layer - newly added
+my_first_nn.add(Dense(5, activation='relu')) # hidden layer - newly added
+my_first_nn.add(Dense(1, activation='sigmoid')) # output layer
+my_first_nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=100,
+                                     initial_epoch=0)
+print(my_first_nn.summary())
+print(my_first_nn.evaluate(X_test, Y_test))
+
+
+# question 2 
+
+import pandas
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+
+# load dataset
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+
+dataset = pd.read_csv("breastcancer.csv")
+dataset['diagnosis'] = dataset['diagnosis'].map({'M': 1, 'B': 0}) #converting categorical to numeric data 
+
+X_train, X_test, Y_train, Y_test = train_test_split(dataset[:,2:32], dataset[:,1],
+                                                    test_size=0.25, random_state=87)
+np.random.seed(155)
+my_first_nn = Sequential() # create model
+my_first_nn.add(Dense(20, input_dim=30, activation='relu')) # hidden layer
+my_first_nn.add(Dense(10, activation='relu')) # hidden layer - newly added
+my_first_nn.add(Dense(5, activation='relu')) # hidden layer - newly added
+my_first_nn.add(Dense(1, activation='sigmoid')) # output layer
+my_first_nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=100,
+                                     initial_epoch=0)
+print(my_first_nn.summary())
+print(my_first_nn.evaluate(X_test, Y_test))
+
+
+# question 3 
+
+import pandas
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from sklearn.preprocessing import StandardScaler # for scaling operation
+
+# load dataset
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+
+dataset = pd.read_csv("breastcancer.csv")
+dataset['diagnosis'] = dataset['diagnosis'].map({'M': 1, 'B': 0})
+
+#scaling 
+scaler = StandardScaler()
+dataset.iloc[:,2:32] = scaler.fit_transform(dataset.iloc[:,2:32])
+
+X_train, X_test, Y_train, Y_test = train_test_split(dataset[:,2:32], dataset[:,1],
+                                                    test_size=0.25, random_state=87)
+np.random.seed(155)
+my_first_nn = Sequential() # create model
+my_first_nn.add(Dense(20, input_dim=30, activation='relu')) # hidden layer
+my_first_nn.add(Dense(10, activation='relu')) # hidden layer - newly added
+my_first_nn.add(Dense(5, activation='relu')) # hidden layer - newly added
+my_first_nn.add(Dense(1, activation='sigmoid')) # output layer
+my_first_nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=100,
+                                     initial_epoch=0)
+print(my_first_nn.summary())
+print(my_first_nn.evaluate(X_test, Y_test))
+
